e7f605b54236b0cb9400f16be7c06dc332540508
leoziondev <leozionsuporte@gmail.com>
2024-11-26 16:26:35 -0300

    feat: delete candidato by passing via props

--- src/components/candidato/CandidatoItem.tsx
+++ src/components/candidato/CandidatoItem.tsx
@@ -1,26 +1,19 @@
-'use client'
-
 import Candidato from "@/data/model/Candidato"
 import Button from "../shared/Button";
 
 interface CandidatoProps {
     candidato: Candidato;
+    onDelete: (candidato: Candidato) => void
 }
 
-export default function CandidatoItem({ candidato }: CandidatoProps) {
-    
-    const handleDelete = (candidato: Candidato) => {
-        console.log(candidato);
-        
-    }
-
+export default function CandidatoItem({ candidato, onDelete }: CandidatoProps) {    
     return (
         <div className="bg-zinc-800 p-5 rounded-lg">
             <h2>{candidato.nome} ({candidato.partido})</h2>
             <p>{candidato.descricao}</p>
             <div className="mt-2">
                 <Button
-                    onClick={() => handleDelete(candidato)}
+                    onClick={() => onDelete(candidato)}
                     className="bg-red-500 hover:bg-red-600"
                 >
                     Excluir
--- src/components/candidato/CandidatoList.tsx
+++ src/components/candidato/CandidatoList.tsx
@@ -1,18 +1,27 @@
-import Candidato from "@/data/model/Candidato";
+'use client'
+
+import Candidato from "@/data/model/Candidato"
 import CandidatoItem from "./CandidatoItem";
+import { useState } from "react";
 
 export interface CandidatosProps {
     candidatos: Candidato[];
 }
 
 export default function CandidatoList({candidatos}: CandidatosProps) {
+    const [allCandidatos, setAllCandidatos] = useState(candidatos)
+
+    function handleDelete(candidato: Candidato) {
+        setAllCandidatos((prev) => prev.filter((c) => c.id !== candidato.id))      
+    }
 
     return (
         <div className="flex flex-col gap-5">
-            {candidatos.map((candidato) => (
+            {allCandidatos.map((candidato) => (
                 <CandidatoItem
                     key={candidato.id}
                     candidato={candidato}
+                    onDelete={handleDelete}
                 />
             ))}
         </div>
