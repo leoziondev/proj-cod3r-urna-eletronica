4be132b737497eff38d986c7e0adc6bc91bc4b33
leoziondev <leozionsuporte@gmail.com>
2024-11-26 15:15:35 -0300

    feat: refactor to admin layout

--- src/app/(admin)/candidato/page.tsx
+++ src/app/(admin)/candidato/page.tsx
@@ -1,14 +1,11 @@
 import CandidatoList from "@/components/candidato/CandidatoList";
 import Container from "@/components/shared/Container";
-import LayoutVotacao from "@/components/shared/LayoutVotacao";
 import candidatos from "@/data/constants/candidatos";
 
 export default function PageCandidato()  {
-    return (
-        <LayoutVotacao>
-            <Container className="py-8">
-                <CandidatoList candidatos={candidatos} />
-            </Container>
-        </LayoutVotacao>
+    return (        
+        <Container className="py-8">
+            <CandidatoList candidatos={candidatos} />
+        </Container>        
     )
 }
---
+++ src/app/(admin)/eleitor/page.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/shared/Container";
+
+export default function PAgeEleitor() {
+    return (
+        <Container>
+            <h1>Eleitor</h1>
+        </Container>
+    )
+}
