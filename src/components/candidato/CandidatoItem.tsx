4754f72e9a1bac70054fbd5b08f21a9805942482
leoziondev <leozionsuporte@gmail.com>
2024-11-26 17:38:36 -0300

    feat: add edit action candidato item component

--- src/components/candidato/CandidatoItem.tsx
+++ src/components/candidato/CandidatoItem.tsx
@@ -4,20 +4,27 @@ import Button from "../shared/Button";
 interface CandidatoProps {
     candidato: Candidato;
     onDelete: (candidato: Candidato) => void
+    onSelect: (candidato: Candidato) => void
 }
 
-export default function CandidatoItem({ candidato, onDelete }: CandidatoProps) {    
+export default function CandidatoItem({ candidato, onDelete, onSelect }: CandidatoProps) {    
     return (
         <div className="bg-zinc-800 p-5 rounded-lg">
             <h2>{candidato.nome} ({candidato.partido})</h2>
             <p>{candidato.descricao}</p>
-            <div className="mt-2">
+            <div className="flex items-center gap-2 mt-2">
+                <Button
+                    onClick={() => onSelect(candidato)}
+                    className="bg-sky-500 hover:bg-sky-600"
+                >
+                    Editar
+                </Button>
                 <Button
                     onClick={() => onDelete(candidato)}
                     className="bg-red-500 hover:bg-red-600"
                 >
                     Excluir
-                </Button>
+                </Button>                
             </div>
         </div>
     )
