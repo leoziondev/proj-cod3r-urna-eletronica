07c4318c28a041917658ca413c68eb52ba5c1974
leoziondev <leozionsuporte@gmail.com>
2024-11-26 15:03:04 -0300

    feat: fix classes

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,14 +1,15 @@
+import Link from "next/link";
 import Container from "./Container";
 
 export default function Header() {
     return (
-        <header className="h-24 border-b border-zinc-800 px-5">
+        <header className="border-b border-zinc-800">
             <Container className="flex justify-between items-center">
                 <h1 className="font-bold text-xl">Eleições 2024</h1>
                 <nav className="flex gap-5">
-                    <a href="#" className="text-white">Início</a>
-                    <a href="#" className="text-white">Candidatos</a>
-                    <a href="#" className="text-white">Partidos</a>
+                    <Link href="/" className="text-white">Início</Link>
+                    <Link href="/candidato" className="text-white">Candidatos</Link>
+                    <Link href="/eleitor" className="text-white">Eleitor</Link>
                 </nav>
             </Container>
         </header>
