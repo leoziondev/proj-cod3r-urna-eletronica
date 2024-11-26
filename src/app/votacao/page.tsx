851ebcad536cf627e48c396d46e57fb897be7297
leoziondev <leozionsuporte@gmail.com>
2024-11-26 16:17:25 -0300

    feat: create page votacao

---
+++ src/app/votacao/page.tsx
@@ -0,0 +1,9 @@
+export default function Votacao() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <div className="bg-zinc-900 w-full max-w-5xl rounded-xl min-h-[450px]">
+                Votação
+            </div>
+        </div>
+    )
+}
