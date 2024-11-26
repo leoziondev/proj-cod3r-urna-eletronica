45d1fc59762621d0c4017a99005132e3d436ac39
leoziondev <leozionsuporte@gmail.com>
2024-11-26 15:03:38 -0300

    feat: apply layout votacao in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import LayoutVotacao from "@/components/shared/LayoutVotacao";
+
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Projeto Urna Eletrônica</h1>
-    </div>
+    <LayoutVotacao>
+      <div className="flex items-center justify-center h-full">
+        <h1 className="text-4xl font-bold">Projeto Urna Eletrônica</h1>
+      </div>
+    </LayoutVotacao>
   );
 }
